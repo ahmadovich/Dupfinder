8ba5048b35d8ca9e3744b33eef4fcba00fa94f4c
Ahmad Hamdy <ahmad.hamdy@gmail.com>
2018-06-13 14:35:51 +0200

    Working version of dupfinder
    dupfinder is now working as single processed program
    prints duplicate files

--- dupfinder.py
+++ dupfinder.py
@@ -5,10 +5,9 @@ Normal version, no multiprocessing
 import argparse
 from collections import defaultdict
 import os
-from multiprocessing import Pool
 from hashlib import sha1
 
-
+# Collecting command line arguments
 parser = argparse.ArgumentParser('subtitles')
 parser.add_argument('-d', '--dir', required = True, type = str, nargs = '+', metavar = '', help = 'Directories to search')
 parser.add_argument('-R' , '--Remove', required = False, action = 'store_true', help = 'Remove duplicates')
@@ -16,26 +15,40 @@ parser.add_argument('-S' , '--Sync', required = False, action = 'store_true', he
 
 args = parser.parse_args()
 
-def gethash(curdir,file):
-    hasher = sha1()
-    filepath = os.path.join(curdir,file)
-    with open(filepath,'rb') as file_to_hash:
-        buf = file_to_hash.read(65536)
-        hasher.update(buf)
-        return {hasher.hexdigest():filepath}
+def gethash():
     
-def main():
-    dict1 = {}
-    for i in args.dir:
-        if not os.path.isdir(i):
-            print('\n' + i + ' is not a directory, please check\n')
+    # Create a result dictionary, default values are lists
+    resdict = defaultdict(list)
+    # Check if arguments are directories
+    for dirz in args.dir:
+        if not os.path.isdir(dirz):
+            print('\n' + dirz + ' is not a directory, please check\n')
             exit(1)
         
-    for (curdir, subdirs, filenames) in os.walk(args.dir[0]) :
+        # Walk through the directory structure
+        for (curdir, subdirs, filenames) in os.walk(dirz) :
             for file in filenames:
-                dict1.update(gethash(curdir,file))
-                
-    print(dict1)
-            
+                # Join filename to and path to generate full path
+                filepath = os.path.join(curdir,file).encode('utf8')
+                with open(filepath,'rb') as file_to_hash:
+                    # Create/reset a hasher to calculate sha1 for each file
+                    hasher = sha1()
+                    buf = file_to_hash.read(65536)
+                    hasher.update(buf)
+                    resdict[hasher.hexdigest()].append(filepath)
+    return resdict
+                    
+        
     
+def main():
+    
+    duplicates = 0
+    result = gethash()
+    for i in result.keys():
+        if len(result[i]) > 1 :
+            duplicates += 1
+            print(result[i])
+    print(duplicates)
+            
+  
 if __name__ == '__main__': main()
